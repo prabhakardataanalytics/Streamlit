7a1809e3e420a0f579c2e3f4906ca09716825470
Prabhakar <65151654+prabhakardataanalytics@users.noreply.github.com>
2023-11-25 20:43:35 +0530

    Add files via upload

---
+++ plotly.py
@@ -0,0 +1,26 @@
+# Core package
+import streamlit as st
+
+# Load EDA Pkg
+import pandas as pd
+import numpy as np
+
+
+# Load Data Vis Pkg
+import plotly.express as px
+
+
+def main():
+    st.title("Plotting In Streamlit with Plotly")
+    df = pd.read_csv("prog_languages_data.csv")
+    st.dataframe(df)
+    
+    fig = px.pie(df,values='Sum',names='lang',title='P')
+    st.plotly_chart(fig)
+    
+    fig2= px.bar(df,x='lang',y='Sum')
+    st.plotly_chart(fig2)
+    
+    
+if __name__ =='__main__':
+    main()
